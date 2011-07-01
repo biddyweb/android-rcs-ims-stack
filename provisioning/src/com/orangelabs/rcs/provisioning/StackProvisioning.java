05965d525b187505f7c73cd348a19e9a48a7835e
jmauffret <jmauffret@gmail.com>
2011-07-01 12:42:33 +0000

    v2.2.7 provisioning

--- provisioning/src/com/orangelabs/rcs/provisioning/StackProvisioning.java
+++ provisioning/src/com/orangelabs/rcs/provisioning/StackProvisioning.java
@@ -18,9 +18,8 @@
 
 package com.orangelabs.rcs.provisioning;
 
-import java.util.Map;
-
-import javax.sip.ListeningPoint;
+import com.orangelabs.rcs.provider.settings.RcsSettings;
+import com.orangelabs.rcs.provider.settings.RcsSettingsData;
 
 import android.app.Activity;
 import android.content.ContentResolver;
@@ -35,12 +34,13 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.orangelabs.rcs.provider.settings.RcsSettings;
-import com.orangelabs.rcs.provider.settings.RcsSettingsData;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Map;
 
 /**
  * Stack parameters provisioning
- * 
+ *
  * @author jexa7410
  */
 public class StackProvisioning extends Activity {
@@ -48,42 +48,42 @@ public class StackProvisioning extends Activity {
 	 * SIP protocol
 	 */
     private static final String[] SIP_PROTOCOL = {
-    	ListeningPoint.UDP, ListeningPoint.TCP
+        "UDP", "TCP", "TLS"
     };
-    
+
 	/**
 	 * Network access
 	 */
     private static final String[] NETWORK_ACCESS = {
     	"All networks", "Mobile only", "Wi-Fi only"
     };
-    
+
     /**
 	 * Content resolver
 	 */
 	private ContentResolver cr;
-    
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         // Set layout
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.stack_provisioning);
-        
+
     	// Set database content resolver
         this.cr = getContentResolver();
 
-        
+
     }
 
     @Override
     protected void onResume() {
     	super.onResume();
-    	
+
     	// Get settings from database
         Map<String, String> settings = RcsSettings.getInstance().dump();
-        
+
         // Display stack parameters
         Spinner spinner = (Spinner)findViewById(R.id.NetworkAccess);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
@@ -99,15 +99,65 @@ public class StackProvisioning extends Activity {
             spinner.setSelection(0);
         }
 
-        spinner = (Spinner)findViewById(R.id.SipDefaultProtocol);
+        spinner = (Spinner) findViewById(R.id.SipDefaultProtocolForMobile);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, SIP_PROTOCOL);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        if (RcsSettings.getInstance().getSipDefaultProtocolForMobile().equalsIgnoreCase(SIP_PROTOCOL[0])) {
+            spinner.setSelection(0);
+        } else if (RcsSettings.getInstance().getSipDefaultProtocolForMobile().equalsIgnoreCase(SIP_PROTOCOL[1])) {
+            spinner.setSelection(1);
+        } else {
+            spinner.setSelection(2);
+        }
+
+        spinner = (Spinner) findViewById(R.id.SipDefaultProtocolForWifi);
         adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, SIP_PROTOCOL);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-        if (RcsSettings.getInstance().getSipDefaultProtocol().equalsIgnoreCase(SIP_PROTOCOL[0])) {
+        if (RcsSettings.getInstance().getSipDefaultProtocolForWifi().equalsIgnoreCase(SIP_PROTOCOL[0])) {
             spinner.setSelection(0);
-        } else {
+        } else if (RcsSettings.getInstance().getSipDefaultProtocolForWifi().equalsIgnoreCase(SIP_PROTOCOL[1])) {
             spinner.setSelection(1);
+        } else {
+            spinner.setSelection(2);
+        }
+
+
+        String[] certificates = loadCertificatesList();
+
+        spinner = (Spinner) findViewById(R.id.TlsCertificateRoot);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, certificates);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        boolean found = false;
+        for (int i = 0; i < certificates.length; i++) {
+            if (RcsSettings.getInstance().getTlsCertificateRoot().equals(certificates[i])) {
+                spinner.setSelection(i);
+                found = true;
+            }
+        }
+        if (!found) {
+            spinner.setSelection(0);
+            Provisioning.writeParameter(cr, "TlsCertificateRoot", "");
+        }
+
+        spinner = (Spinner) findViewById(R.id.TlsCertificateIntermediate);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, certificates);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setSelection(0);
+        found = false;
+        for (int i = 0; i < certificates.length; i++) {
+            if (RcsSettings.getInstance().getTlsCertificateIntermediate().equals(certificates[i])) {
+                spinner.setSelection(i);
+                found = true;
+            }
+        }
+        if (!found) {
+            spinner.setSelection(0);
+            Provisioning.writeParameter(cr, "TlsCertificateIntermediate", "");
         }
 
         EditText txt = (EditText)this.findViewById(R.id.ImsConnectionPollingPeriod);
@@ -115,22 +165,25 @@ public class StackProvisioning extends Activity {
 
         txt = (EditText)this.findViewById(R.id.ImsServicePollingPeriod);
         txt.setText(settings.get("ImsServicePollingPeriod"));
-        
+
         txt = (EditText)this.findViewById(R.id.SipListeningPort);
         txt.setText(settings.get("SipListeningPort"));
-        
+
         txt = (EditText)this.findViewById(R.id.SipTransactionTimeout);
         txt.setText(settings.get("SipTransactionTimeout"));
-        
+
+        txt = (EditText)this.findViewById(R.id.SipKeepAlivePeriod);
+        txt.setText(settings.get("SipKeepAlivePeriod"));
+
         txt = (EditText)this.findViewById(R.id.DefaultMsrpPort);
         txt.setText(settings.get("DefaultMsrpPort"));
 
 	    txt = (EditText)this.findViewById(R.id.DefaultRtpPort);
 	    txt.setText(settings.get("DefaultRtpPort"));
-            
+
 		txt = (EditText)this.findViewById(R.id.MsrpTransactionTimeout);
 		txt.setText(settings.get("MsrpTransactionTimeout"));
-		
+
         txt = (EditText)this.findViewById(R.id.RegisterExpirePeriod);
         txt.setText(settings.get("RegisterExpirePeriod"));
 
@@ -139,55 +192,75 @@ public class StackProvisioning extends Activity {
 
         txt = (EditText)this.findViewById(R.id.RevokeTimeout);
         txt.setText(settings.get("RevokeTimeout"));
-    		
+
         txt = (EditText)this.findViewById(R.id.RingingPeriod);
         txt.setText(settings.get("RingingPeriod"));
-    		
+
         txt = (EditText)this.findViewById(R.id.SubscribeExpirePeriod);
         txt.setText(settings.get("SubscribeExpirePeriod"));
-    		
+
         txt = (EditText)this.findViewById(R.id.IsComposingTimeout);
         txt.setText(settings.get("IsComposingTimeout"));
-    		
+
         txt = (EditText)this.findViewById(R.id.SessionRefreshExpirePeriod);
         txt.setText(settings.get("SessionRefreshExpirePeriod"));
-    	
+
         txt = (EditText)this.findViewById(R.id.CapabilityRefreshTimeout);
         txt.setText(settings.get("CapabilityRefreshTimeout"));
-        
+
         txt = (EditText)this.findViewById(R.id.CapabilityExpiryTimeout);
         txt.setText(settings.get("CapabilityExpiryTimeout"));
 
         txt = (EditText)this.findViewById(R.id.CapabilityPollingPeriod);
         txt.setText(settings.get("CapabilityPollingPeriod"));
 
-    	CheckBox check = (CheckBox)this.findViewById(R.id.PermanentState);
+    	CheckBox check = (CheckBox)this.findViewById(R.id.SipKeepAlive);
+        check.setChecked(Boolean.parseBoolean(settings.get("SipKeepAlive")));
+
+        check = (CheckBox)this.findViewById(R.id.PermanentState);
         check.setChecked(Boolean.parseBoolean(settings.get("PermanentState")));
 
     	check = (CheckBox)this.findViewById(R.id.TelUriFormat);
         check.setChecked(Boolean.parseBoolean(settings.get("TelUriFormat")));
-        
+
     	check = (CheckBox)this.findViewById(R.id.ImAlwaysOn);
         check.setChecked(Boolean.parseBoolean(settings.get("ImAlwaysOn")));
 
     	check = (CheckBox)this.findViewById(R.id.ImUseReports);
         check.setChecked(Boolean.parseBoolean(settings.get("ImUseReports")));
     }
-    
+
     @Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater=new MenuInflater(getApplicationContext());
 		inflater.inflate(R.menu.main_menu, menu);
 		return true;
 	}
-    
+
     @Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 			case R.id.menu_save:
 		        // Save stack parameters
-				Spinner spinner = (Spinner)findViewById(R.id.SipDefaultProtocol);
-				Provisioning.writeParameter(cr, "SipDefaultProtocol", (String)spinner.getSelectedItem());
+				Spinner spinner = (Spinner)findViewById(R.id.SipDefaultProtocolForMobile);
+				Provisioning.writeParameter(cr, "SipDefaultProtocolForMobile", (String)spinner.getSelectedItem());
+
+				spinner = (Spinner)findViewById(R.id.SipDefaultProtocolForWifi);
+                Provisioning.writeParameter(cr, "SipDefaultProtocolForWifi", (String)spinner.getSelectedItem());
+
+                spinner = (Spinner) findViewById(R.id.TlsCertificateRoot);
+                if (spinner.getSelectedItemPosition() == 0)
+                    Provisioning.writeParameter(cr, "TlsCertificateRoot", "");
+                else
+                    Provisioning.writeParameter(cr, "TlsCertificateRoot",
+                        (String) spinner.getSelectedItem());
+
+                spinner = (Spinner) findViewById(R.id.TlsCertificateIntermediate);
+                if (spinner.getSelectedItemPosition() == 0)
+                    Provisioning.writeParameter(cr, "TlsCertificateIntermediate", "");
+                else
+                    Provisioning.writeParameter(cr, "TlsCertificateIntermediate",
+                        (String) spinner.getSelectedItem());
 
 				spinner = (Spinner)findViewById(R.id.NetworkAccess);
 				int index = spinner.getSelectedItemPosition();
@@ -201,26 +274,29 @@ public class StackProvisioning extends Activity {
 					case 2:
 						Provisioning.writeParameter(cr, "NetworkAccess", ""+RcsSettingsData.WIFI_ACCESS);
 						break;
-				}					
-				
+                }
+
 				EditText txt = (EditText)this.findViewById(R.id.ImsConnectionPollingPeriod);
 				Provisioning.writeParameter(cr, "ImsConnectionPollingPeriod", txt.getText().toString());
-				
+
 		        txt = (EditText)this.findViewById(R.id.SipListeningPort);
 				Provisioning.writeParameter(cr, "SipListeningPort", txt.getText().toString());
-		        
+
 		        txt = (EditText)this.findViewById(R.id.SipTransactionTimeout);
 				Provisioning.writeParameter(cr, "SipTransactionTimeout", txt.getText().toString());
-		        
-		        txt = (EditText)this.findViewById(R.id.DefaultMsrpPort);
+
+		        txt = (EditText)this.findViewById(R.id.SipKeepAlivePeriod);
+				Provisioning.writeParameter(cr, "SipKeepAlivePeriod", txt.getText().toString());
+
+				txt = (EditText)this.findViewById(R.id.DefaultMsrpPort);
 				Provisioning.writeParameter(cr, "DefaultMsrpPort", txt.getText().toString());
 
 			    txt = (EditText)this.findViewById(R.id.DefaultRtpPort);
 				Provisioning.writeParameter(cr, "DefaultRtpPort", txt.getText().toString());
-		            
+
 				txt = (EditText)this.findViewById(R.id.MsrpTransactionTimeout);
 				Provisioning.writeParameter(cr, "MsrpTransactionTimeout", txt.getText().toString());
-				
+
 		        txt = (EditText)this.findViewById(R.id.RegisterExpirePeriod);
 				Provisioning.writeParameter(cr, "RegisterExpirePeriod", txt.getText().toString());
 
@@ -229,19 +305,19 @@ public class StackProvisioning extends Activity {
 
 		        txt = (EditText)this.findViewById(R.id.RevokeTimeout);
 				Provisioning.writeParameter(cr, "RevokeTimeout", txt.getText().toString());
-		    		
+
 		        txt = (EditText)this.findViewById(R.id.RingingPeriod);
 				Provisioning.writeParameter(cr, "RingingPeriod", txt.getText().toString());
-		    		
+
 		        txt = (EditText)this.findViewById(R.id.SubscribeExpirePeriod);
 				Provisioning.writeParameter(cr, "SubscribeExpirePeriod", txt.getText().toString());
-		    		
+
 		        txt = (EditText)this.findViewById(R.id.IsComposingTimeout);
 				Provisioning.writeParameter(cr, "IsComposingTimeout", txt.getText().toString());
-		    		
+
 		        txt = (EditText)this.findViewById(R.id.SessionRefreshExpirePeriod);
 				Provisioning.writeParameter(cr, "SessionRefreshExpirePeriod", txt.getText().toString());
-		    	
+
 		        txt = (EditText)this.findViewById(R.id.CapabilityRefreshTimeout);
 				Provisioning.writeParameter(cr, "CapabilityRefreshTimeout", txt.getText().toString());
 
@@ -251,7 +327,10 @@ public class StackProvisioning extends Activity {
 				txt = (EditText)this.findViewById(R.id.CapabilityPollingPeriod);
 				Provisioning.writeParameter(cr, "CapabilityPollingPeriod", txt.getText().toString());
 
-		    	CheckBox check = (CheckBox)this.findViewById(R.id.PermanentState);
+		    	CheckBox check = (CheckBox)this.findViewById(R.id.SipKeepAlive);
+				Provisioning.writeParameter(cr, "SipKeepAlive", Boolean.toString(check.isChecked()));
+
+				check = (CheckBox)this.findViewById(R.id.PermanentState);
 				Provisioning.writeParameter(cr, "PermanentState", Boolean.toString(check.isChecked()));
 
 		    	check = (CheckBox)this.findViewById(R.id.TelUriFormat);
@@ -262,10 +341,47 @@ public class StackProvisioning extends Activity {
 
 		    	check = (CheckBox)this.findViewById(R.id.ImUseReports);
 				Provisioning.writeParameter(cr, "ImUseReports", Boolean.toString(check.isChecked()));
-				
-				Toast.makeText(this, getString(R.string.label_reboot_service), Toast.LENGTH_LONG).show();				
+
+                Toast.makeText(this, getString(R.string.label_reboot_service), Toast.LENGTH_LONG)
+                        .show();
 				break;
 		}
 		return true;
 	}
+
+    /**
+     * Load a list of certificate file in the sd card
+     * 
+     * @return
+     */
+    private String[] loadCertificatesList() {
+        String[] files = null;
+        File folder = new File(RcsSettingsData.CERTIFICATE_FOLDER_PATH);
+        try {
+            folder.mkdirs();
+            if (folder.exists()) {
+                // filter
+                FilenameFilter filter = new FilenameFilter() {
+                    public boolean accept(File dir, String filename) {
+                        return filename.contains(RcsSettingsData.CERTIFICATE_FILE_TYPE);
+                    }
+                };
+                files = folder.list(filter);
+            }
+        } catch (SecurityException e) {
+            // intentionally blank
+        }
+        // Add an empty string on first position
+        if (files == null) {
+            return new String[] {
+                getString(R.string.label_no_certificate)
+            };
+        } else {
+            String[] temp = new String[files.length + 1];
+            temp[0] = getString(R.string.label_no_certificate);
+            if (files.length > 0)
+                System.arraycopy(files, 0, temp, 1, files.length);
+            return temp;
+        }
+    }
 }
