b533ddafa162fc97b41a273857f2f6acf49d8af1
jmauffret <jmauffret@gmail.com>
2011-08-18 14:28:54 +0000

    update extension demo

---
+++ test/extension/src/com/orangelabs/rcs/extension/WhiteBoard.java
@@ -0,0 +1,13 @@
+package com.orangelabs.rcs.extension;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WhiteBoard extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_wb);
+    }    
+}
