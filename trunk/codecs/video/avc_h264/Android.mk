7fd4e4e7a10deb54caa725891728d668178f1f2e
jmauffret@gmail.com <jmauffret@gmail.com@09ec735a-148d-11df-8ebe-87dcbb8c21ba>
2013-07-09 19:33:31 +0000

    remove codecs source code. This part is independent from the stack and should be in the application part.

--- trunk/codecs/video/avc_h264/Android.mk
+++
@@ -1,3 +0,0 @@
-AVC_ROOT:= $(call my-dir)
-#include $(AVC_ROOT)/colorconvert/Android.mk
-include $(call all-subdir-makefiles)
--- trunk/codecs/video/avc_h264/Application.mk
+++
@@ -1,2 +0,0 @@
-APP_ABI := armeabi
-APP_MODULES := colorconvert libH264Decoder libH264Encoder
