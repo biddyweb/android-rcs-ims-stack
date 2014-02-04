b40f460927a81564a2dcf4c824081de6b799c587
lemordant.philippe@gmail.com <lemordant.philippe@gmail.com@09ec735a-148d-11df-8ebe-87dcbb8c21ba>
2014-02-04 14:09:07 +0000

    Add test of SDP for Chat and File Transfer Correct test for Video SDP and Geoloc.

--- trunk/core/tests/src/com/orangelabs/rcs/geoloc/GeolocTest.java
+++ trunk/core/tests/src/com/orangelabs/rcs/geoloc/GeolocTest.java
@@ -1,10 +1,12 @@
 package com.orangelabs.rcs.geoloc;
 
 import com.orangelabs.rcs.service.api.client.messaging.GeolocPush;
+import com.orangelabs.rcs.utils.logger.Logger;
 
 import android.test.AndroidTestCase;
 
 public class GeolocTest extends AndroidTestCase {
+	
 	protected void setUp() throws Exception {
 		super.setUp();
 	}
@@ -17,19 +19,19 @@ public class GeolocTest extends AndroidTestCase {
 		// GeolocPush g = new GeolocPush(label, latitude, longitude, altitude, expiration);
 		GeolocPush g = new GeolocPush("label", 1.0, 2.0, 3.0, 0, 100);
 		String str = GeolocPush.formatGeolocToStr(g);
-		assertEquals(str, "label,1.0,2.0,3.0,0,100");
+		assertEquals(str, "label,1.0,2.0,3.0,0,100.0");
 		
 		g = new GeolocPush("label", 1.0, 2.0, 3.0, 0);
 		str = GeolocPush.formatGeolocToStr(g);
-		assertEquals(str, "label,1.0,2.0,3.0,0,0");
+		assertEquals(str, "label,1.0,2.0,3.0,0,0.0");
 
 		g = new GeolocPush("", 1.0, 2.0, 3.0, 0, 100);
 		str = GeolocPush.formatGeolocToStr(g);
-		assertEquals(str, ",1.0,2.0,3.0,0,100");
+		assertEquals(str, ",1.0,2.0,3.0,0,100.0");
 		
 		g = new GeolocPush(null, 1.0, 2.0, 3.0, 0, 100);
 		str = GeolocPush.formatGeolocToStr(g);
-		assertEquals(str, ",1.0,2.0,3.0,0,100");
+		assertEquals(str, ",1.0,2.0,3.0,0,100.0");
 	}
 
 }
