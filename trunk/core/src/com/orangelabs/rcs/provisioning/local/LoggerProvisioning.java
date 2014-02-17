cbd17a8b4a9f0307382550b154185d8582a50e45
lemordant.philippe@gmail.com <lemordant.philippe@gmail.com@09ec735a-148d-11df-8ebe-87dcbb8c21ba>
2014-02-17 13:41:59 +0000

    Allow orientation change for manual provisioning

--- trunk/core/src/com/orangelabs/rcs/provisioning/local/LoggerProvisioning.java
+++ trunk/core/src/com/orangelabs/rcs/provisioning/local/LoggerProvisioning.java
@@ -18,22 +18,22 @@
 
 package com.orangelabs.rcs.provisioning.local;
 
+import static com.orangelabs.rcs.provisioning.local.Provisioning.saveCheckBoxParameter;
+import static com.orangelabs.rcs.provisioning.local.Provisioning.saveEditTextParameter;
+import static com.orangelabs.rcs.provisioning.local.Provisioning.setCheckBoxParameter;
+import static com.orangelabs.rcs.provisioning.local.Provisioning.setEditTextParameter;
 import android.app.Activity;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.orangelabs.rcs.R;
 import com.orangelabs.rcs.provider.settings.RcsSettings;
 import com.orangelabs.rcs.provider.settings.RcsSettingsData;
-import com.orangelabs.rcs.utils.logger.Logger;
 
 /**
  * End user profile parameters provisioning
@@ -49,56 +49,33 @@ public class LoggerProvisioning extends Activity {
     };
 	
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
         
         // Set layout
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.rcs_provisioning_logger);
         
 		// Set buttons callback
         Button btn = (Button)findViewById(R.id.save_btn);
-        btn.setOnClickListener(saveBtnListener);        
-	}
-	
-	@Override
-	protected void onResume() {
-		super.onResume();
-		
-        // Display parameters
-    	CheckBox check = (CheckBox)this.findViewById(R.id.TraceActivated);
-        check.setChecked(RcsSettings.getInstance().isTraceActivated());
+        btn.setOnClickListener(saveBtnListener);    
         
-    	check = (CheckBox)this.findViewById(R.id.SipTraceActivated);
-        check.setChecked(RcsSettings.getInstance().isSipTraceActivated());
+        // Display parameters
+		setCheckBoxParameter(this, R.id.TraceActivated, RcsSettingsData.TRACE_ACTIVATED, bundle);
+		setCheckBoxParameter(this, R.id.SipTraceActivated, RcsSettingsData.SIP_TRACE_ACTIVATED, bundle);
+		setCheckBoxParameter(this, R.id.MediaTraceActivated, RcsSettingsData.MEDIA_TRACE_ACTIVATED, bundle);
+		setEditTextParameter(this, R.id.SipTraceFile, RcsSettingsData.SIP_TRACE_FILE, bundle);
 
-		check = (CheckBox)this.findViewById(R.id.MediaTraceActivated);
-        check.setChecked(RcsSettings.getInstance().isMediaTraceActivated());
-
-		EditText txt = (EditText)this.findViewById(R.id.SipTraceFile);
-		txt.setText(RcsSettings.getInstance().readParameter(RcsSettingsData.SIP_TRACE_FILE));
-
-		Spinner spinner = (Spinner)findViewById(R.id.TraceLevel);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, TRACE_LEVEL);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        int level = RcsSettings.getInstance().getTraceLevel();
-        if (level == Logger.DEBUG_LEVEL) {
-            spinner.setSelection(0);
-        } else
-        if (level == Logger.INFO_LEVEL) {
-            spinner.setSelection(1);
-        } else
-        if (level == Logger.WARN_LEVEL) {
-            spinner.setSelection(2);
-        } else
-        if (level == Logger.ERROR_LEVEL) {
-            spinner.setSelection(3);
-        } else
-        if (level == Logger.FATAL_LEVEL) {
-            spinner.setSelection(4);
-        }
+		Spinner spinner = (Spinner) findViewById(R.id.TraceLevel);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, TRACE_LEVEL);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adapter);
+		Integer parameter = null;
+		if (bundle != null && bundle.containsKey(RcsSettingsData.TRACE_LEVEL)) {
+			parameter = bundle.getInt(RcsSettingsData.TRACE_LEVEL);
+		} else {
+			parameter = RcsSettings.getInstance().getTraceLevel();
+		}
+		spinner.setSelection(parameter);
 	}
 
     /**
@@ -111,42 +88,34 @@ public class LoggerProvisioning extends Activity {
         }
     };
     
+    @Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		saveInstanceState(outState);
+	}
+    
+	/**
+	 * Save parameters either in bundle or in RCS settings
+	 */
+    private void saveInstanceState( Bundle bundle) {
+		saveCheckBoxParameter(this, R.id.TraceActivated, RcsSettingsData.TRACE_ACTIVATED, bundle);
+		saveCheckBoxParameter(this, R.id.SipTraceActivated, RcsSettingsData.SIP_TRACE_ACTIVATED, bundle);
+		saveCheckBoxParameter(this, R.id.MediaTraceActivated, RcsSettingsData.MEDIA_TRACE_ACTIVATED, bundle);
+		saveEditTextParameter(this, R.id.SipTraceFile, RcsSettingsData.SIP_TRACE_FILE, bundle);
+		Spinner spinner = (Spinner) findViewById(R.id.TraceLevel);
+		if (bundle != null) {
+			bundle.putInt(RcsSettingsData.TRACE_LEVEL, spinner.getSelectedItemPosition());
+		} else {
+			Integer value = spinner.getSelectedItemPosition();
+			RcsSettings.getInstance().writeParameter(RcsSettingsData.TRACE_LEVEL, value.toString());
+		}
+	}
+    
     /**
      * Save parameters
      */
     private void save() {	
-        CheckBox check = (CheckBox)this.findViewById(R.id.TraceActivated);
-		RcsSettings.getInstance().writeParameter(RcsSettingsData.TRACE_ACTIVATED, Boolean.toString(check.isChecked()));
-
-        check = (CheckBox)this.findViewById(R.id.SipTraceActivated);
-        RcsSettings.getInstance().writeParameter(RcsSettingsData.SIP_TRACE_ACTIVATED, Boolean.toString(check.isChecked()));
-
-		check = (CheckBox)this.findViewById(R.id.MediaTraceActivated);
-        RcsSettings.getInstance().writeParameter(RcsSettingsData.MEDIA_TRACE_ACTIVATED, Boolean.toString(check.isChecked()));
-
-		Spinner spinner = (Spinner)findViewById(R.id.TraceLevel);
-		String value = (String)spinner.getSelectedItem();
-        int level = Logger.ERROR_LEVEL;
-        if (value.equals(TRACE_LEVEL[0])) {
-            level = Logger.DEBUG_LEVEL;
-        } else
-        if (value.equals(TRACE_LEVEL[1])) {
-            level = Logger.INFO_LEVEL;
-        } else
-        if (value.equals(TRACE_LEVEL[2])) {
-            level = Logger.WARN_LEVEL;
-        } else
-        if (value.equals(TRACE_LEVEL[3])) {
-            level = Logger.ERROR_LEVEL;
-        } else
-        if (value.equals(TRACE_LEVEL[4])) {
-            level = Logger.FATAL_LEVEL;
-        }
-		RcsSettings.getInstance().writeParameter(RcsSettingsData.TRACE_LEVEL, ""+level);
-
-		EditText txt = (EditText)this.findViewById(R.id.SipTraceFile);
-		RcsSettings.getInstance().writeParameter(RcsSettingsData.SIP_TRACE_FILE, txt.getText().toString());
-
+    	saveInstanceState(null);
 		Toast.makeText(this, getString(R.string.label_reboot_service), Toast.LENGTH_LONG).show();				
 	}
 }
