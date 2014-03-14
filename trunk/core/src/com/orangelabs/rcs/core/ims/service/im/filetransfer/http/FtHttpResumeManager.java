22caecb2b090d42eaf24a0fec0891429ca02f9f0
lemordant.philippe@gmail.com <lemordant.philippe@gmail.com@09ec735a-148d-11df-8ebe-87dcbb8c21ba>
2014-03-14 14:03:45 +0000

    Ft HTTP resume manager for download

--- trunk/core/src/com/orangelabs/rcs/core/ims/service/im/filetransfer/http/FtHttpResumeManager.java
+++ trunk/core/src/com/orangelabs/rcs/core/ims/service/im/filetransfer/http/FtHttpResumeManager.java
@@ -17,12 +17,17 @@
  ******************************************************************************/
 package com.orangelabs.rcs.core.ims.service.im.filetransfer.http;
 
+import java.util.LinkedList;
 import java.util.List;
 
+import com.orangelabs.rcs.core.content.ContentManager;
+import com.orangelabs.rcs.core.content.MmContent;
 import com.orangelabs.rcs.core.ims.service.im.InstantMessagingService;
+import com.orangelabs.rcs.core.ims.service.im.filetransfer.FileSharingError;
 import com.orangelabs.rcs.core.ims.service.im.filetransfer.FileSharingSessionListener;
 import com.orangelabs.rcs.provider.fthttp.FtHttpResume;
 import com.orangelabs.rcs.provider.fthttp.FtHttpResumeDaoImpl;
+import com.orangelabs.rcs.provider.fthttp.FtHttpResumeDownload;
 import com.orangelabs.rcs.provider.fthttp.FtHttpStatus;
 import com.orangelabs.rcs.utils.logger.Logger;
 
@@ -33,7 +38,10 @@ public class FtHttpResumeManager {
 	private FtHttpResumeDaoImpl dao = FtHttpResumeDaoImpl.getInstance();
 	private boolean terminate = false;
 	private InstantMessagingService imsService;
-	private List<FtHttpResume> ftHttp2Resume;
+	/**
+	 * List of pending sessions to resume 
+	 */
+	private LinkedList<FtHttpResume> ftHttp2Resume;
 	FileSharingSessionListener listener;
 	/**
 	 * The logger
@@ -57,8 +65,12 @@ public class FtHttpResumeManager {
 		try {
 			// delete entries in FT HTTP which are no more useful
 			dao.clean();
-			ftHttp2Resume = dao.queryAll(FtHttpStatus.STARTED);
-			processNext();
+			// Retrieve all pending sessions
+			List<FtHttpResume> listFile2resume = dao.queryAll(FtHttpStatus.STARTED);
+			if (listFile2resume.isEmpty() == false) {
+				ftHttp2Resume = new LinkedList<FtHttpResume>(listFile2resume);
+				processNext();
+			}
 		} catch (Exception e) {
 			// handle exception
 			if (logger.isActivated()) {
@@ -68,31 +80,85 @@ public class FtHttpResumeManager {
 
 	}
 
-	private void processNext() {
-		// if (ftHttp2Resume.isEmpty())
-		// return;
-		// FtHttpResume ftHttpResume = ftHttp2Resume.ftHttp2Resume
-		// if (ftHttpResume != null) {
-		// switch (ftHttpResume.getDirection()) {
-		// case INCOMING:
-		// FtHttpResumeDownload download = (FtHttpResumeDownload) ftHttpResume;
-		// MmContent content = ContentManager.createMmContentFromMime(download.getFilename(), download.getUrl(),
-		// download.getMimeType(), download.getSize());
-		// final ResumeDownloadFileSharingSession resumeDownload = new ResumeDownloadFileSharingSession(imsService, content,
-		// ftHttpResume.getContact(), ftHttpResume.getThumbnail(), ftHttpResume.getSessionId(),
-		// ftHttpResume.getChatId(), download);
-		// // resumeDownload.addListener(this);
-		// new Thread() {
-		// public void run() {
-		// resumeDownload.start();
-		// }
-		// }.start();
-		// break;
-		// case OUTGOING:
-		// // TODO
-		// break;
-		// }
-		// }
+	/**
+	 * resume next pending session
+	 */
+	synchronized private void processNext() {
+		listener = null;
+		if (ftHttp2Resume.isEmpty())
+			return;
+		// Remove the oldest session from the list
+		FtHttpResume ftHttpResume = ftHttp2Resume.poll();
+		if (logger.isActivated()) {
+			logger.debug("Resume FT HTTP "+ftHttpResume);
+		}
+		switch (ftHttpResume.getDirection()) {
+		case INCOMING:
+			FtHttpResumeDownload download = (FtHttpResumeDownload) ftHttpResume;
+			MmContent content = ContentManager.createMmContentFromMime(download.getFilename(), download.getUrl(),
+					download.getMimeType(), download.getSize());
+			// Creates the Resume Download session object
+			final ResumeDownloadFileSharingSession resumeDownload = new ResumeDownloadFileSharingSession(imsService, content,
+					ftHttpResume.getContact(), ftHttpResume.getThumbnail(), ftHttpResume.getSessionId(), ftHttpResume.getChatId(),
+					download);
+			listener = getFileSharingSessionListener();
+			resumeDownload.addListener(listener);
+			new Thread() {
+				public void run() {
+					resumeDownload.start();
+				}
+			}.start();
+			break;
+		case OUTGOING:
+			// TODO
+			break;
+		}
+
+	}
+
+	private FileSharingSessionListener getFileSharingSessionListener() {
+		return new FileSharingSessionListener() {
+
+			@Override
+			public void handleSessionTerminatedByRemote() {
+				if (listener != null)
+					processNext();
+			}
+
+			@Override
+			public void handleSessionStarted() {
+			}
+
+			@Override
+			public void handleSessionAborted(int reason) {
+				if (listener != null)
+					processNext();
+			}
+
+			@Override
+			public void handleTransferProgress(long currentSize, long totalSize) {
+			}
+
+			@Override
+			public void handleTransferError(FileSharingError error) {
+				if (listener != null)
+					processNext();
+			}
+
+			@Override
+			public void handleFileTransfered(String filename) {
+				if (listener != null)
+					processNext();
+			}
+
+			@Override
+			public void handleFileTransferResumed() {
+			}
+
+			@Override
+			public void handleFileTransferPaused() {
+			}
+		};
 	}
 
 	public void terminate() {
