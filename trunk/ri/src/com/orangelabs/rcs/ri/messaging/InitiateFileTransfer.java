21b5b49b1b3ed1ccef91e90061ebd87615a5182a
lemordant.philippe@gmail.com <lemordant.philippe@gmail.com@09ec735a-148d-11df-8ebe-87dcbb8c21ba>
2014-03-27 08:56:14 +0000

    Issue 257: Failed to get real path from URI with Android KitKat
    Upgrade target to API 19 (Kitkat)
    Use Document ID to retrieve file path if Kitkat
    
    Fixes issue 257

--- trunk/ri/src/com/orangelabs/rcs/ri/messaging/InitiateFileTransfer.java
+++ trunk/ri/src/com/orangelabs/rcs/ri/messaging/InitiateFileTransfer.java
@@ -23,6 +23,7 @@ import java.io.File;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnCancelListener;
@@ -31,11 +32,14 @@ import android.content.pm.ActivityInfo;
 import android.database.Cursor;
 import android.database.MatrixCursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.RemoteException;
 import android.provider.ContactsContract;
 import android.provider.ContactsContract.Data;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -58,6 +62,7 @@ import com.orangelabs.rcs.service.api.client.contacts.ContactsApi;
 import com.orangelabs.rcs.service.api.client.messaging.IFileTransferEventListener;
 import com.orangelabs.rcs.service.api.client.messaging.IFileTransferSession;
 import com.orangelabs.rcs.service.api.client.messaging.MessagingApi;
+import com.orangelabs.rcs.utils.logger.Logger;
 
 /**
  * Initiate file transfer
@@ -106,6 +111,9 @@ public class InitiateFileTransfer extends Activity {
      * Progress dialog
      */
     private Dialog progressDialog = null;
+    
+    /** The logger */
+    private final static Logger logger = Logger.getLogger(InitiateFileTransfer.class.getSimpleName());
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -392,6 +400,9 @@ public class InitiateFileTransfer extends Activity {
 
 		case SELECT_IMAGE:
 		case SELECT_TEXT_FILE:
+			if (logger.isActivated()) {
+				logger.debug("Selected file uri:" +uri);
+			}
 			// Get filename
 			filename = getFilePath(this, uri);
 			// Display the selected filename attribute
@@ -401,8 +412,11 @@ public class InitiateFileTransfer extends Activity {
 				sizeEdit.setText(filesize + " KB");
 				uriEdit.setText( file.getName());
 			} catch (Exception e) {
+				if (logger.isActivated()) {
+					logger.error("Exception occurred",e);
+				}
 				filesize = -1;
-				sizeEdit.setText(R.string.label_file_size_unknown);
+				sizeEdit.setText("Unknown");
 				uriEdit.setText(filename);
 			}
 			// Show invite button
@@ -421,25 +435,112 @@ public class InitiateFileTransfer extends Activity {
      * @return the File path
      */
 	public static String getFilePath(final Context context, final Uri uri) {
-		if ("content".equalsIgnoreCase(uri.getScheme())) {
-			String[] projection = { MediaStore.Images.ImageColumns.DATA };
-			Cursor cursor = null;
+		final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
 
-			try {
-				cursor = context.getContentResolver().query(uri, projection, null, null, null);
-				int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.DATA);
-				if (cursor.moveToFirst()) {
-					return cursor.getString(column_index);
+		// DocumentProvider
+		if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+			// ExternalStorageProvider
+			if (isExternalStorageDocument(uri)) {
+				final String docId = DocumentsContract.getDocumentId(uri);
+				final String[] split = docId.split(":");
+				final String type = split[0];
+
+				if ("primary".equalsIgnoreCase(type)) {
+					return Environment.getExternalStorageDirectory() + "/" + split[1];
 				}
-			} catch (Exception e) {
-			} finally {
-				if (cursor != null)
-					cursor.close();
+
+				// TODO handle non-primary volumes
 			}
+			// DownloadsProvider
+			else if (isDownloadsDocument(uri)) {
+
+				final String id = DocumentsContract.getDocumentId(uri);
+				final Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"),
+						Long.valueOf(id));
+
+				return getDataColumn(context, contentUri, null, null);
+			}
+			// MediaProvider
+			else if (isMediaDocument(uri)) {
+				final String docId = DocumentsContract.getDocumentId(uri);
+				final String[] split = docId.split(":");
+				final String type = split[0];
+
+				Uri contentUri = null;
+				if ("image".equals(type)) {
+					contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+				} else if ("video".equals(type)) {
+					contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+				} else if ("audio".equals(type)) {
+					contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+				}
+
+				final String selection = "_id=?";
+				final String[] selectionArgs = new String[] { split[1] };
+
+				return getDataColumn(context, contentUri, selection, selectionArgs);
+			}
+		}
+		if ("content".equalsIgnoreCase(uri.getScheme())) {
+			return getDataColumn(context, uri, null, null);
 		} else if ("file".equalsIgnoreCase(uri.getScheme())) {
 			return uri.getPath();
 		}
+		return null;
+	}
+	
+	/**
+	 * @param uri The Uri to check.
+	 * @return Whether the Uri authority is ExternalStorageProvider.
+	 */
+	public static boolean isExternalStorageDocument(Uri uri) {
+	    return "com.android.externalstorage.documents".equals(uri.getAuthority());
+	}
 
+	/**
+	 * @param uri The Uri to check.
+	 * @return Whether the Uri authority is DownloadsProvider.
+	 */
+	public static boolean isDownloadsDocument(Uri uri) {
+	    return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+	}
+
+	/**
+	 * @param uri The Uri to check.
+	 * @return Whether the Uri authority is MediaProvider.
+	 */
+	public static boolean isMediaDocument(Uri uri) {
+	    return "com.android.providers.media.documents".equals(uri.getAuthority());
+	}
+	
+	/**
+	 * Get the value of the data column for this Uri. This is useful for MediaStore Uris, and other file-based ContentProviders.
+	 * 
+	 * @param context
+	 *            The context.
+	 * @param uri
+	 *            The Uri to query.
+	 * @param selection
+	 *            (Optional) Filter used in the query.
+	 * @param selectionArgs
+	 *            (Optional) Selection arguments used in the query.
+	 * @return The value of the _data column, which is typically a file path.
+	 */
+	public static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
+		Cursor cursor = null;
+		final String column = "_data";
+		final String[] projection = { column };
+
+		try {
+			cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+			if (cursor != null && cursor.moveToFirst()) {
+				final int column_index = cursor.getColumnIndexOrThrow(column);
+				return cursor.getString(column_index);
+			}
+		} finally {
+			if (cursor != null)
+				cursor.close();
+		}
 		return null;
 	}
 
