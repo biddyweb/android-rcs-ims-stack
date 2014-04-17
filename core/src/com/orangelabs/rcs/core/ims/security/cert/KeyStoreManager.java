01ff84ec6c8542d2b7dc734207b7acfcd7e6fdab
lemordant.philippe <lemordant.philippe@gmail.com>
2014-04-17 08:13:11 +0000

    Replace <CR><LF> by <LF> to allow simple comparison

--- core/src/com/orangelabs/rcs/core/ims/security/cert/KeyStoreManager.java
+++ core/src/com/orangelabs/rcs/core/ims/security/cert/KeyStoreManager.java
@@ -22,7 +22,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FilenameFilter;
-import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigInteger;
 import java.security.KeyPair;
