50e797c02345909cbfc210f4f78c765cf9f62430
lemordant.philippe <lemordant.philippe@gmail.com>
2014-08-06 12:39:20 +0000

    Issue 261: 	Exception if P-Asserted-Identity includes the display name in the Chat invitation Fixes issue 261

--- core/src/com/orangelabs/rcs/core/ims/network/sip/SipUtils.java
+++ core/src/com/orangelabs/rcs/core/ims/network/sip/SipUtils.java
@@ -43,7 +43,6 @@ import javax2.sip.message.Request;
 import com.orangelabs.rcs.core.TerminalInfo;
 import com.orangelabs.rcs.core.ims.protocol.sip.SipMessage;
 import com.orangelabs.rcs.core.ims.protocol.sip.SipRequest;
-import com.orangelabs.rcs.utils.PhoneUtils;
 
 /**
  * SIP utility functions
@@ -338,22 +337,22 @@ public class SipUtils {
 				// Get value of the first header
 				assertedHeader1 = ((ExtensionHeader) list.next()).getValue();
 				if (assertedHeader1.contains("tel:")) {
-					return PhoneUtils.cleanUriHeadingTrailingChar(assertedHeader1);
+					return assertedHeader1;
 				}
 				if (list.hasNext()) {
 					// Get value of the second header (it may not be present)
 					String assertedHeader2 = ((ExtensionHeader) list.next())
 							.getValue();
 					if (assertedHeader2.contains("tel:")) {
-						return PhoneUtils.cleanUriHeadingTrailingChar(assertedHeader2);
+						return assertedHeader2;
 					}
 				}
 				// In case there is no tel uri, return the value of the first header
-				return PhoneUtils.cleanUriHeadingTrailingChar(assertedHeader1);
+				return assertedHeader1;
 			}
 		}
 		// No P-AssertedIdentity header, we take the value in the FROM uri
-		return PhoneUtils.cleanUriHeadingTrailingChar(request.getFromUri());
+		return request.getFromUri();
 	}
 	
     /**
